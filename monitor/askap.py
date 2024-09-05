3776c6a18d6d79d854438c3f353b200f7a635a22
epaell <elenc@me.com>
2024-09-06 07:17:43 +1000

    Cleaned up and fixed based on service changes

--- monitor/askap.py
+++ monitor/askap.py
@@ -10,34 +10,6 @@ from time import sleep
 from astropy.table import Table, vstack
 from astropy.io import ascii
 
-def read_known_footprints():
-    footprints = {}
-    footprints["VAST"] = ["square_6x6", 1.05, 45.0]
-    footprints["EMU"] = ["closepack36", 0.90, 45.0]
-    footprints["RACS"] = ["closepack36", 0.90, 45.0]
-    
-    fields = []
-    for fname in ["vast_fields.csv", "emu_fields.csv", "racs_fields.csv"]:
-        sst_data = Table.read(fname, format='csv')
-#        sst_data = sst_data[np.where(sst_data["SBID"]>0)]
-        if len(fields) == 0:
-            fields = sst_data
-        else:
-            fields = vstack([fields, sst_data])
-    return fields, footprints
-
-def get_footprint(fields, footprints, field_name):
-    field = fields[np.where(fields["FIELD_NAME"]==field_name)]
-    if len(field) == 0:
-        return 0.0, 0.0, "", 0.0, 0.0
-    # FIELD_NAME,RA_HMS,DEC_DMS,RA_DEG,DEC_DEG,GAL_LONG,GAL_LAT,POL_AXIS
-    field = field[0]
-    sst = field_name.split("_")[0]
-    footprint = footprints[sst][0]
-    pitch = footprints[sst][1]
-    rotation = footprints[sst][2]
-    return field["RA_DEG"], field["DEC_DEG"], footprint, pitch, field["POL_AXIS"] + rotation
-    
 def get_askap_data():
     url = "https://prod-api.vlbi.atnf.tools/get_status/askap"
     response = urlopen(url)
@@ -45,10 +17,13 @@ def get_askap_data():
     return data, json.loads(data)
 
 def dump_raw(data):
-    keys = ['infoTime', 'antennasList', 'template', 'configuration', 'azimuth', 'state', 'alias', 'antennaName', 'declinationICRF', 'rightAscensionICRF', 'progress', 'errors', 'antennasIn', 'elevation', 'id', 'duration', 'stateError', 'weather', 'status']
-    for key in keys:
-        print(key, " : ", data[key])
-        print()
+    for key1 in data.keys():
+        dataval = data[key1]
+        if type(dataval) is dict:
+            for key2 in data[key1].keys():
+                print("  %s : " %(key2), dataval[key2])
+        else:
+            print("%s : " %(key1), dataval)
 
 def median_ra_dec(data):
     ra_str = np.array(data['rightAscensionICRF'])
@@ -92,49 +67,58 @@ def get_states(data):
         obs_status = "Slewing"
     return obs_status, ant_states
 
-def dump_summary(fields, footprints, start_time, current_time, field_name, sbid, coord, pol, duration, progress, missing_ants, obs_status, ant_states, status, weather, footprint):
-    print("Current time (UTC): %s" %(current_time))
-    print("Observing status: %s" %(obs_status))
+def flatten(data):
+    fdata = {}
+    for key1 in data.keys():
+        dataval = data[key1]
+        if type(dataval) is dict:
+            for key2 in data[key1].keys():
+                fdata[key2] = dataval[key2]
+        else:
+            fdata[key1] = dataval
+    fdata["medianAzimuth"] = np.median(fdata["azimuth"])
+    obs_status, ant_states = get_states(fdata)
+    fdata["obsStatus"] = obs_status
+    fdata["antStates"] = ant_states
+    missing = find_missing(fdata)
+    fdata["missingAnts"] = missing
+    fdata["coord"] = median_ra_dec(fdata)
+    fdata["sbid"] = int(fdata["id"])
+    # Convert these key value to float
+    floats = ["duration", "progress", "pol_axis_angle", "temperature", "humidity", "pressure", "windSpeed"]
+    for key in floats:
+        fdata[key] = float(fdata[key])
+    return fdata
+
+def dump_summary(fdata):
+    print("Current time (UTC): %s" %(fdata["infoTime"]))
+    print("Observing status: %s" %(fdata["obsStatus"]))
     # Observing parameters only make sense when not in Stowed state
-    if (obs_status in ["Stowed"]) == False:
-        print("Currently observing: %s" %(field_name))
-        print("Scheduling block: %d" %(sbid))
-        print("Coordinate: %s" %(coord.to_string(style='hmsdms')))
-        print("Total duration %.1f s" %(duration))
-        print("Progress: %.1f %%" %(progress))
-    #    print("%d tracking; %d slewing; %d idle; %d stowed" %(ant_states["Tracking"], ant_states["Slewing"], ant_states["Idle"], ant_states["Stowed"]))
-        if len(missing_ants) == 0:
+    if (fdata["obsStatus"] in ["Stowed"]) == False:
+        print("Currently observing: %s" %(fdata["field_name"]))
+        print("Scheduling block: %d" %(fdata["sbid"]))
+        print("Coordinate: %s" %(fdata["coord"].to_string(style='hmsdms')))
+        print("Start time: %s" %(fdata["startTime"]))
+        print("Total duration %.1f s" %(fdata["duration"]))
+        print("Progress: %.1f %%" %(fdata["progress"]))
+
+        if len(fdata["missingAnts"]) == 0:
             print("All antennas available")
         else:
-            print("Missing antennas: %s" %(", ".join(missing_ants)))
+            print("Missing antennas: %s" %(", ".join(fdata["missingAnts"])))
 
-        fra_deg, fdec_deg, ffootprint, fpitch, frotation = get_footprint(fields, footprints, field_name)
-        print("ffootprint=",ffootprint)
-        sc = SkyCoord(Angle(fra_deg, unit=u.deg), Angle(fdec_deg, unit=u.deg), frame='fk5')
-    
         print("Footprint:")
-        print("  Footprint    : %s" %(footprint["name"]))
-        print("  Pitch        : %.2f" %(footprint["pitch"]))
-        print("  Rotation     : %.1f" %(footprint["rotation"]))
-        print("  Polarisation : %.1f" %(pol))
-        if ffootprint == "":
-            print("Unknown DB footprint")
-        else:
-            print("DB Footprint:")
-            print("  Coordinate  : %s" %(sc.to_string(style='hmsdms')))
-            print("  Footprint   : %s" %(ffootprint))
-            print("  Pitch       : %.2f" %(fpitch))
-            print("  Rotation    : %.1f" %(frotation))
-#    print("status: %s" %(status))
-#    print("stateError: %s" %(stateError))
-    print("Weather conditions")
-    print("  Temperature: %.1f C" %(weather["temperature"]))
-    print("  Humidty    : %.1f %%" %(weather["humidity"]))
-    print("  Pressure   : %.1f millibars" %(weather["pressure"]))
-    print("  WindSpeed. : %.1f kph" %(weather["windSpeed"]))
-    print("  StormFlag. : %s" %(weather["stormFlag"]))
+        print("  Footprint    : %s" %(fdata["name"]))
+        print("  Pitch        : %.2f" %(fdata["pitch"]))
+        print("  Rotation     : %.1f" %(fdata["rotation"]+fdata["pol_axis_angle"]))
 
-fields, footprints = read_known_footprints()
+    print("Weather conditions")
+    print("  Temperature: %.1f C" %(fdata["temperature"]))
+    print("  Humidty    : %.1f %%" %(fdata["humidity"]))
+    print("  Pressure   : %.1f millibars" %(fdata["pressure"]))
+    print("  WindSpeed. : %.1f kph" %(fdata["windSpeed"]))
+    print("  StormFlag. : %s" %(fdata["stormFlag"]))
+
 summary = True
 log = True
 while True:
@@ -151,22 +135,6 @@ while True:
         sys.exit(0)
 
     # Extract what appears to be more useful information from the data provided
-    sbid = int(data['id'])
-    field_name = data['alias']
-    current_time = data['infoTime']
-    coord = median_ra_dec(data)
-    pol = np.median(data['polarisation'])
-
-    progress = data['progress']
-    duration = data['duration']
-    stateError = data['stateError']
-    status = data['status']
-    weather = data['weather']
-    footprint = data["footprint"]
-    start_time = data["startTime"]
-
-    missing_ants = find_missing(data)
-    obs_status, ant_states = get_states(data)
-    
-    dump_summary(fields, footprints, start_time, current_time, field_name, sbid, coord, pol, duration, progress, missing_ants, obs_status, ant_states, status, weather, footprint)
+    fdata = flatten(data)    
+    dump_summary(fdata)
     sleep(60)
